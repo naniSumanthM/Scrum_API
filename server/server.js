4e7c806ac36dbcf47f95e79eb4c24c2cfaf6d7ea
Sumanth Maddirala <sumanth@Sumanths-MacBook-Pro.local>
2018-07-07 17:57:25 -0400

    routes protected

--- server/server.js
+++ server/server.js
@@ -11,113 +11,6 @@ let { authenticate } = require("./middleware/authenticate");
 let app = express();
 app.use(bodyParser.json());
 
-//POST - Create New Userstory
-app.post("/userstories", (req, res) => {
-  let userstory = new Userstory({
-    title: req.body.title,
-    description: req.body.description,
-    status: req.body.status
-  });
-
-  userstory.save().then(
-    doc => {
-      res.status(200).send(doc);
-      console.log(req.body);
-    },
-    e => {
-      res.status(400).send(e);
-    }
-  );
-});
-
-//GET - fetches all user stories
-app.get("/userstories", (req, res) => {
-  Userstory.find().then(
-    userstories => {
-      res.status(200).send({
-        userstories
-      });
-    },
-    e => {
-      res.status(400).send(e);
-    }
-  );
-});
-
-//GET - fetches individiaul user stories
-app.get("/userstories/:id", (req, res) => {
-  let id = req.params.id;
-
-  if (!ObjectId.isValid(id)) {
-    return res.status(404).send();
-  }
-
-  Userstory.findById(id)
-    .then(userstory => {
-      if (!userstory) {
-        res.status(404).send();
-      } else {
-        res.send({
-          userstory
-        });
-      }
-    })
-    .catch(e => {
-      res.status(400).send();
-    });
-});
-
-//DELETE - purges a single document given an _id
-app.delete("/userstories/:id", (req, res) => {
-  let id = req.params.id;
-
-  if (!ObjectId.isValid(id)) {
-    return res.status(404).send();
-  }
-
-  Userstory.findByIdAndRemove(id)
-    .then(userstory => {
-      if (!userstory) {
-        return res.status(404).send();
-      }
-      res.send(userstory);
-    })
-    .catch(e => {
-      res.status(400).send();
-    });
-});
-
-//UPDATE - Update documents in MongoDB
-app.patch("/userstories/:id", (req, res) => {
-  let id = req.params.id;
-  let body = _.pick(req.body, ["title", "description", "status"]);
-
-  if (!ObjectId.isValid(id)) {
-    res.status(404).send;
-  }
-
-  Userstory.findByIdAndUpdate(
-    id,
-    {
-      $set: body
-    },
-    {
-      new: true
-    }
-  )
-    .then(userstory => {
-      if (!userstory) {
-        return res.status(404).send();
-      }
-      res.send({
-        userstory
-      });
-    })
-    .catch(e => {
-      res.status(400).send();
-    });
-});
-
 // POST /users
 // returns a header which contains the jSON web token
 app.post("/users", (req, res) => {
@@ -158,7 +51,6 @@ app.post("/users/login", (req, res) => {
 });
 
 //DELETE method - logout
-
 app.delete("/users/me/token", authenticate, (req, res) => {
   req.user.removeToken(req.token).then(
     () => {
@@ -168,6 +60,114 @@ app.delete("/users/me/token", authenticate, (req, res) => {
       res.status(400).send();
     }
   );
+});
+
+//POST - Create New Userstory
+app.post("/userstories", authenticate, (req, res) => {
+  let userstory = new Userstory({
+    title: req.body.title,
+    description: req.body.description,
+    status: req.body.status,
+    _creator: req.user._id
+  });
+
+  userstory.save().then(
+    doc => {
+      res.status(200).send(doc);
+    },
+    e => {
+      res.status(400).send(e);
+    }
+  );
+});
+
+//GET - fetches all user stories
+app.get("/userstories", authenticate, (req, res) => {
+  Userstory.find({ _creator: req.user._id }).then(
+    userstories => {
+      res.status(200).send({
+        userstories
+      });
+    },
+    e => {
+      res.status(400).send(e);
+    }
+  );
+});
+
+//GET - fetches individiaul user stories
+app.get("/userstories/:id", authenticate, (req, res) => {
+  let id = req.params.id;
+
+  if (!ObjectId.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Userstory.findOne({
+    _id: id,
+    _creator: req.user._id
+  })
+    .then(userstory => {
+      if (!userstory) {
+        res.status(404).send();
+      } else {
+        res.send({
+          userstory
+        });
+      }
+    })
+    .catch(e => {
+      res.status(400).send();
+    });
+});
+
+//DELETE - purges a single document given an _id
+app.delete("/userstories/:id", authenticate, (req, res) => {
+  let id = req.params.id;
+
+  if (!ObjectId.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Userstory.findOneAndRemove({
+    _id: id,
+    _creator: req.user._id
+  })
+    .then(userstory => {
+      if (!userstory) {
+        return res.status(404).send();
+      }
+      res.send(userstory);
+    })
+    .catch(e => {
+      res.status(400).send();
+    });
+});
+
+//UPDATE - Update documents in MongoDB
+app.patch("/userstories/:id", authenticate, (req, res) => {
+  let id = req.params.id;
+  let body = _.pick(req.body, ["title", "description", "status"]);
+
+  if (!ObjectId.isValid(id)) {
+    res.status(404).send;
+  }
+
+  Userstory.findOneAndUpdate(
+    { _id: id, _creator: req.user._id },
+    { $set: body },
+    { new: true }
+  )
+    .then(userstory => {
+      if (!userstory) {
+        return res.status(404).send();
+      }
+
+      res.send({ userstory });
+    })
+    .catch(e => {
+      res.status(400).send();
+    });
 });
 
 //run server on localhost
